7037fd8fb18cc2873a8e29ebe275f1697d7592f2
Tinotenda Chingaira <36966940+ChinXXy@users.noreply.github.com>
2023-07-12 10:43:59 +0200

    Update login.component.ts

--- src/app/views/auth/login/login.component.ts
+++ src/app/views/auth/login/login.component.ts
@@ -21,9 +21,9 @@ export class LoginComponent implements OnInit {
   public isLoggedIn = false;
   isLoginFailed = false;
   constructor(
-    private authService: AuthService,
-    private tokenStorage: TokenStorageService,
-    private router: Router
+     //private authService: AuthService,
+    // private tokenStorage: TokenStorageService,
+    // private router: Router
     ) { }
 
 
@@ -31,27 +31,28 @@ export class LoginComponent implements OnInit {
     console.log("tugd");
     localStorage.clear();
     const { username, password } = this.form;
-    this.authService.login(username, password, companyDB).subscribe(
-      (data) => {
-        this.userInfo = JSON.parse(data.success);
-        console.log(data.error);
+    // this.authService.login(username, password, companyDB).subscribe(
+    //   (data) => {
+    //     this.userInfo = JSON.parse(data.success);
+    //     console.log(data.error);
 
-        console.log(data);
+    //     console.log(data);
 
-        for (let x = 0; x < 1; x++) {
-          if (data.success !== null) {
-            this.userInfo[x] = JSON.parse(data.success);
-          }
-          if (data.error !== null) {
-            // swal("Error", data.error, "error");
-          }
-          localStorage.setItem("Session", this.userInfo[x].Session);
-          // swal("User Logged In", "Welcome", "success");
-        }
+    //     for (let x = 0; x < 1; x++) {
+    //       if (data.success !== null) {
+    //         this.userInfo[x] = JSON.parse(data.success);
+    //       }
+    //       if (data.error !== null) {
+    //         // swal("Error", data.error, "error");
+    //       }
+    //       localStorage.setItem("Session", this.userInfo[x].Session);
+    //       // swal("User Logged In", "Welcome", "success");
+    //     }
 
-        this.router.navigateByUrl('/dashboard');      },
+    //     //this.router.navigateByUrl('/dashboard');      
+    //   },
       
-    );
+    //);
   }
   ngOnInit(): void { }
 
